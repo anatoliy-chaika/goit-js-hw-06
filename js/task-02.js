7eb5b7ff307676b86b93c5d8bd17f6dfb1471820
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2022-12-07 17:14:09 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const getUl = document.querySelector("#ingredients")
+
+
+const createMarcup = ingredients.map((item, index) => {
+const newMarcup = document.createElement("li");
+newMarcup.classList.add("item");
+newMarcup.textContent = ingredients[index];
+return newMarcup
+})
+getUl.append(...createMarcup)
+
+
+
+
+
+
