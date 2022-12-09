151d121c1b6b3856afb2cf654129b33d6eb322b6
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2022-12-09 12:34:58 +0200

    done... 9.12.2022 12:34

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+const buttonChange = document.querySelector(".change-color");
+
+buttonChange.addEventListener("click", getResult);
+
+function getResult(event) {
+  span.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+  console.log(body);
+}
